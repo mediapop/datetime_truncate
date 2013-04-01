7d4beb999ec710289c7493cc08392ed18f89bb2c
Björn Andersson <ba@sanitarium.se>
2013-04-01 12:40:21 +0200

    The changes that made the install work for PyPi

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 # -*- encoding: utf-8 -*-
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name='datetime_truncate',
-    version='1.0.2',
+    version='1.0.0',
     url='https://github.com/mediapop/datetime_truncate',
     author='Björn Andersson / Media Pop',
     author_email='bjorn@mediapop.co',
     description='Truncate datetime objects to a set level of precision',
     license='BSD',
     long_description=open('README.rst').read(),
-    packages=find_packages(exclude=('tests',)),
+    packages=['datetime_truncate'],
     package_data={
         '': ['README.rst']
-        },
+    },
     include_package_data=True,
     test_suite='nose.collector',
     tests_require=['nose>=1.2.1'],
